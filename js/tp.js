346a3e98deb2661da84e471bbaeb16e5f36e571b
moattez.07@gmail.com <moattez.07@gmail.com>
2019-03-23 17:59:23 +0100

    lien vers to do

---
+++ js/tp.js
@@ -0,0 +1,9 @@
+var x=document.getElementById(b1);
+x.addEventListener("click",f1);
+function f1()
+{
+    var x1=document.getElementById(i1);
+    x2=document.getElementById(i2);     
+    x1.value="";
+    x2.value="";
+}
