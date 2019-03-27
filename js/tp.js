e8a5adb2867415d70ecaa0f9fc769edb8f68977a
moattez.07@gmail.com <moattez.07@gmail.com>
2019-03-27 14:40:11 +0100

    boo

--- js/tp.js
+++
@@ -1,9 +0,0 @@
-var x=document.getElementById(b1);
-x.addEventListener("click",f1);
-function f1()
-{
-    var x1=document.getElementById(i1);
-    x2=document.getElementById(i2);     
-    x1.value="";
-    x2.value="";
-}
